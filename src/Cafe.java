b6f79f27907e6aa78ec47d343aa96ff2bfeb165c
Lenarte <paulohenriquecof@gmail.com>
2024-04-23 14:12:16 -0300

    Estoque extends Cafe
    Produtos

--- src/Cafe.java
+++ src/Cafe.java
@@ -1,3 +1,31 @@
-public class Cafe {
+public class Cafe extends Estoque {
+
+    private String nomeDoProduto;
+
+    public Cafe () {
+
+    }
     
+    public Cafe(int codigoProduto, String nome, String nomeDoProduto) {
+
+        super(codigoProduto, nome);
+        this.nomeDoProduto = nomeDoProduto;
+
+    }
+
+    public String getNomeDoProduto() {
+        return nomeDoProduto;
+    }
+
+    public void setNomeDoProduto(String nomeDoProduto) {
+        this.nomeDoProduto = nomeDoProduto;
+    }
+
+    @Override
+    public String toString() {
+        
+        return super.toString() + "Nome do Produto: " + nomeDoProduto;
+    }
+
+
 }
