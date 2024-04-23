5f1359dc73f024dbfc89befbd56ca73693fecc11
Lenarte <paulohenriquecof@gmail.com>
2024-04-23 14:03:37 -0300

    CadastroEstoque
    Cadastro para o estoque de produtos

---
+++ src/CadastroEstoque.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CadastroEstoque {
+
+    private static List<Estoque> listaEstoques = new ArrayList<>();
+
+    public static void cadastrar(Estoque estoque) {
+
+        listaEstoques.add(estoque);
+
+    }
+
+    public static List<Estoque> getListaEstoques () {
+
+        return listaEstoques;
+
+    }
+
+    public static Estoque buscar(int codigoProduto) {
+
+        for (Estoque temp: listaEstoques) {
+
+            if (temp.getCodigoProduto() == codigoProduto) {
+
+                return temp;
+                
+            }
+        }
+
+        return null;
+    }
+    
+}
