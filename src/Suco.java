23478e950efebd160f39feffbbd557f2b54a0afc
Fanuel <fanuel.souza31@gmail.com>
2024-04-26 01:50:39 -0300

    class suco
    Estoque extends suco

--- src/Suco.java
+++ src/Suco.java
@@ -1,3 +1,25 @@
-public class Suco {
-    
+public class Suco extends Estoque {
+
+    private String sabor;
+
+    public Suco() {
+    }
+
+    public Suco(int codigoProduto, String nome, String sabor) {
+        super(codigoProduto, nome);
+        this.sabor = sabor;
+    }
+
+    public String getSabor() {
+        return sabor;
+    }
+
+    public void setSabor(String sabor) {
+        this.sabor = sabor;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "Sabor: " + sabor;
+    }
 }
